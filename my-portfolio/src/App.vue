fb55bd75bcf0a10484b8e332ee83753a19c23ce4
64070109Supanat <88423049+JaimesJames@users.noreply.github.com>
2023-07-16 16:49:50 +0700

    kkk

--- my-portfolio/src/App.vue
+++ my-portfolio/src/App.vue
@@ -1,31 +1,64 @@
 <template>
-  <CarouselTemp/>
-  <CarouselTemp/>
+  <div class="bar">
+    <h3>หน้าแรก</h3>
+    <h3>แนะนำตัว</h3>
+    <h3>ค่าย</h3>
+    <h3>ผลงาน</h3>
+    <h3>เหลา</h3>
+  </div>
+  <CarouselTemp />
+
   <h1>ค่าย</h1>
-  <ContentVer1/>
+  <ContentVer1 />
   <h1>ผลงาน</h1>
 </template>
 
 <script>
-import CarouselTemp from './components/CarouselTemp.vue'
-import ContentVer1 from './components/ContentVer1.vue'
+import CarouselTemp from "./components/CarouselTemp.vue";
+import ContentVer1 from "./components/ContentVer1.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     CarouselTemp,
     ContentVer1
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Noto Sans Thai', sans-serif;
+  font-family: "Noto Sans Thai", sans-serif;
   color: #2c3e50;
   text-align: center;
 }
-h1{
+h1 {
   font-size: 100px;
 }
+
+.bar{
+    margin: 20px calc(50% - 35%);
+    width: 70%;
+    height: 50px;
+    display: flex;
+    position: fixed;
+    background-color: hsl(0,50%,100%,20%);
+    border-radius: 50px; 
+}
+
+.bar h3{
+    margin: 0;
+    color: white;
+    font-weight: 400;
+    flex: 1;
+    padding-top: 12px;
+
+ }
+ .bar h3:hover{
+    margin: 0;
+    color: rgb(55, 52, 87);
+    font-weight: 400;
+    flex: 1;
+
+ }
 </style>
--- my-portfolio/src/components/CarouselTemp.vue
+++ my-portfolio/src/components/CarouselTemp.vue
@@ -1,17 +1,10 @@
 <template>
     <div class="container">
-        <div class="bar">
-            <h3>หน้าแรก</h3>
-            <h3>แนะนำตัว</h3>
-            <h3>ค่าย</h3>
-            <h3>ผลงาน</h3>
-            <h3>เหลา</h3>
-        </div>
         <h1>Portfolio</h1>
     </div>
 </template>
 
-<script>
+<script> 
 export default {
     name: 'CarouselTemp'
 }
@@ -22,15 +15,10 @@ export default {
     background-image: url("../assets/test1.jpg");
     background-attachment: fixed;
     width: 100%;
-    height: 30vw;
+    height: 40vw;
     text-align: center;
+    border-radius: 25px;
     
-}
-.bar{
-    padding-top: 1vw;
-    margin: auto;
-    width: 70%;
-    display: flex;
 }
  h1{
     color: rgb(255, 255, 255);
@@ -38,20 +26,6 @@ export default {
     margin: 0;
     font-size: 400px;
     font-weight: 600;
-
- }
- .bar h3{
-    margin: 0;
-    color: white;
-    font-weight: 400;
-    flex: 1;
-
- }
- .bar h3:hover{
-    margin: 0;
-    color: rgb(195, 191, 240);
-    font-weight: 400;
-    flex: 1;
 
  }
 </style>
