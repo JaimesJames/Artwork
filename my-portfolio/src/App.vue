40491c7b62b9d9933b2fbcb28ce298a6f2264539
64070109Supanat <88423049+JaimesJames@users.noreply.github.com>
2023-07-16 16:11:27 +0700

    kk

--- my-portfolio/src/App.vue
+++ my-portfolio/src/App.vue
@@ -1,18 +1,19 @@
 <template>
-  <NavBar/>
+  <CarouselTemp/>
+  <CarouselTemp/>
   <h1>ค่าย</h1>
   <ContentVer1/>
   <h1>ผลงาน</h1>
 </template>
 
 <script>
-import NavBar from './components/NavBar.vue'
+import CarouselTemp from './components/CarouselTemp.vue'
 import ContentVer1 from './components/ContentVer1.vue'
 
 export default {
   name: 'App',
   components: {
-    NavBar,
+    CarouselTemp,
     ContentVer1
   }
 }
@@ -21,8 +22,6 @@ export default {
 <style>
 #app {
   font-family: 'Noto Sans Thai', sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
   text-align: center;
 }
